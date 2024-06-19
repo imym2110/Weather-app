a8333aa87323a15129ea458e37547363a4b7db3e
imym2110 <maniaryash21@gmail.com>
2024-06-19 20:43:33 +0530

    chnaged API call
    chnaged API call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
 import s from "./style.module.css";
-import { BASE_URL, API_KEY_PARAM } from "./config";
-import { WeatherApi } from "./weather-api";
+import { fetchCity } from "./weather-api";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import WbTwilightIcon from "@mui/icons-material/WbTwilight";
-import styled from "@emotion/styled";
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Switch,
-} from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, Switch } from "@mui/material";
 import DarkModeToggle from "react-dark-mode-toggle";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
@@ -34,9 +26,7 @@ function App() {
     const dat = new Date(timestamp * 1000).toLocaleDateString();
     console.log("===><", dat);
     const date = new Date(timestamp * 1000);
-    const formattedHours = (date.getHours() % 12 || 12)
-      .toString()
-      .padStart(2, "0");
+    const formattedHours = (date.getHours() % 12 || 12).toString().padStart(2, "0");
     const formattedMinutes = date.getMinutes().toString().padStart(2, "0");
     const ampm = date.getHours() >= 12 ? "PM" : "AM";
     return `${formattedHours}:${formattedMinutes} ${ampm}`;
@@ -49,18 +39,13 @@ function App() {
         backgroundImage: `url(${isDarkMode ? Nighturl : Dayurl})`,
       }}
     >
-      <DarkModeToggle
-        onChange={setIsDarkMode}
-        checked={isDarkMode}
-        size={70}
-        className={s.toggle}
-      />
+      <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={70} className={s.toggle} />
       <form
         className={s.form}
         onSubmit={async (e) => {
           e.preventDefault();
           if (city) {
-            let res = await WeatherApi.fetchCity(city);
+            let res = await fetchCity(city);
             console.log("hi", res);
             if (res?.status == 200) {
               setTemp(Math.round(res.data.main.temp));
@@ -99,9 +84,7 @@ function App() {
               }
             }}
           >
-            <FavoriteBorderIcon
-              style={{ paddingBottom: "1px", paddingRight: "3px" }}
-            />
+            <FavoriteBorderIcon style={{ paddingBottom: "1px", paddingRight: "3px" }} />
             Favourite
           </button>
         </div>
@@ -114,7 +97,7 @@ function App() {
             onChange={async (e) => {
               e.preventDefault();
               if (city) {
-                let res = await WeatherApi.fetchCity(e.target.value);
+                let res = await fetchCity(e.target.value);
                 console.log("hi", res);
                 if (res?.status == 200) {
                   setTemp(Math.round(res.data.main.temp));
@@ -145,33 +128,13 @@ function App() {
           <h2 className={!isDarkMode ? `${s.para}` : `${s.paraDark}`}>
             {temp}°C/{Math.ceil(temp * 1.8 + 32)}°F in {location}
           </h2>
-          {logo && (
-            <img
-              className={s.weather_image}
-              src={`http://openweathermap.org/img/w/${logo}.png`}
-              alt="icon"
-            />
-          )}
+          {logo && <img className={s.weather_image} src={`http://openweathermap.org/img/w/${logo}.png`} alt="icon" />}
         </div>
       )}
-      {desc && (
-        <h3 className={!isDarkMode ? `${s.desc}` : `${s.descDark}`}>{desc}</h3>
-      )}
-      {feel && (
-        <h4 className={!isDarkMode ? `${s.feel}` : `${s.feelDark}`}>
-          Feels Like : {feel}°C{" "}
-        </h4>
-      )}
-      {humid && (
-        <h4 className={!isDarkMode ? `${s.humidity}` : `${s.humidityDark}`}>
-          Humidity Index : {humid}
-        </h4>
-      )}
-      {windSpeed && (
-        <h4 className={!isDarkMode ? `${s.windSpeed}` : `${s.windSpeedDark}`}>
-          Wind speed : {windSpeed}
-        </h4>
-      )}
+      {desc && <h3 className={!isDarkMode ? `${s.desc}` : `${s.descDark}`}>{desc}</h3>}
+      {feel && <h4 className={!isDarkMode ? `${s.feel}` : `${s.feelDark}`}>Feels Like : {feel}°C </h4>}
+      {humid && <h4 className={!isDarkMode ? `${s.humidity}` : `${s.humidityDark}`}>Humidity Index : {humid}</h4>}
+      {windSpeed && <h4 className={!isDarkMode ? `${s.windSpeed}` : `${s.windSpeedDark}`}>Wind speed : {windSpeed}</h4>}
       <div className={!isDarkMode ? `${s.timing}` : `${s.timingDark}`}>
         {timing.sunrise && (
           <h4 className={s.sunrise}>
--- src/weather-api.js
+++ src/weather-api.js
@@ -1,14 +1,23 @@
 import axios from "axios";
 import { BASE_URL, API_KEY_PARAM, GEO_URL } from "./config";
 
-export class WeatherApi {
-  static async fetchCity(city) {
-    try {
-      console.log("===>");
-      const response = await axios.get(`${BASE_URL}q=${city}${API_KEY_PARAM}`);
-      return response;
-    } catch (error) {
-      alert("City Not Found");
-    }
+// export class WeatherApi {
+//   static async fetchCity(city) {
+//     try {
+//       console.log("===>");
+//       const response = await axios.get(`${BASE_URL}q=${city}${API_KEY_PARAM}`);
+//       return response;
+//     } catch (error) {
+//       alert("City Not Found");
+//     }
+//   }
+// }
+
+export const fetchCity = async (city) => {
+  try {
+    const response = await axios.get(`${BASE_URL}q=${city}${API_KEY_PARAM}`);
+    return response;
+  } catch (error) {
+    alert("City Not Found");
   }
-}
+};
